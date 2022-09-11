ae28374dec4d68f2e571a46a40cfb98086aea124
Emirhan Kayabaş <92752721+emirhankayabas@users.noreply.github.com>
2022-09-11 05:07:00 +0300

    First Upload

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('PATH', realpath("."));
+define('SUBFOLDER', true);
+define('URL', "http://localhost/mvc");
+
+return [
+    'db' => [
+        'name' => "mvc",
+        'host' => "localhost",
+        'user' => "root",
+        'password' => ""
+    ]
+];
